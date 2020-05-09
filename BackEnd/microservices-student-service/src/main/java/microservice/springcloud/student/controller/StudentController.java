4a3c54df5a29f84cab5176ce4420a24ee1d4335e
lorenzo <lor90td@gmail.com>
2020-05-09 02:37:54 -0500

    Save photo student completed

--- BackEnd/microservices-student-service/src/main/java/microservice/springcloud/student/controller/StudentController.java
+++ BackEnd/microservices-student-service/src/main/java/microservice/springcloud/student/controller/StudentController.java
@@ -7,9 +7,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import springcloud.microservices.student.common.model.Student;
 import microservice.springcloud.student.service.StudentServiceImpl;
@@ -39,6 +42,31 @@ public class StudentController extends CommonController<Student, StudentServiceI
 			
 		} catch( Exception err ) {
 			return ResponseEntity.status( HttpStatus.NOT_FOUND ).body( "Error al recuperar los usuarios." );
+		}
+	}
+
+	@PostMapping( "/save-with-photo" )
+	public ResponseEntity<?> saveWithPhoto( @Valid Student student, BindingResult result, @RequestParam MultipartFile file ) {
+		if( result.hasErrors() ) {
+			return validation( result );
+		}	
+		try {
+			return ResponseEntity.status( HttpStatus.CREATED ).body( this.service.saveWithPhoto( student, file ) );
+		} catch( Exception err ) {
+			return ResponseEntity.status( HttpStatus.INTERNAL_SERVER_ERROR ).body( err.getMessage() );
+		}
+	}
+	
+	@PutMapping( "/update-with-photo" )
+	public ResponseEntity<?> updateWithPhoto( @Valid Student student, BindingResult result, @RequestParam MultipartFile file ) {
+		
+		if( result.hasErrors() ) {
+			return validation( result );
+		}
+		try {
+			return ResponseEntity.status( HttpStatus.CREATED ).body( this.service.updateWithPhoto(student, file ) );
+		} catch( Exception err ) {
+			return ResponseEntity.status( HttpStatus.NOT_MODIFIED ).body( err.getMessage() );
 		}
 	}
 }
--- BackEnd/microservices-student-service/src/main/java/microservice/springcloud/student/service/StudentServiceImpl.java
+++ BackEnd/microservices-student-service/src/main/java/microservice/springcloud/student/service/StudentServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import springcloud.microservices.student.common.model.Student;
 import microservice.springcloud.student.repository.StudentRepository;
@@ -33,5 +34,32 @@ public class StudentServiceImpl extends CommonServiceImpl<Student, StudentReposi
 	public List<Student> findByNameOrLastName(String search) {
 		
 		return this.repository.findByNameOrLastName( search );
+	}
+	
+	@Transactional
+	public Student saveWithPhoto( Student student, MultipartFile file ) throws Exception {
+		if( !file.isEmpty() ) {
+			student.setPhoto( file.getBytes() );
+		}
+		return this.repository.save( student );
+	}
+	
+	public Student updateWithPhoto( Student student, MultipartFile file ) throws Exception {
+		
+		if( student.getStudentId() == null ) {
+			throw new Exception( "El id del alumno es obligatorio." );
+		}
+		
+		Optional<Student> oldStudent = this.findById( student.getStudentId() );
+		
+		oldStudent.get().setEmail( student.getEmail() );
+		oldStudent.get().setName( student.getName() );
+		oldStudent.get().setLastName( student.getLastName() );
+		
+		if( !file.isEmpty() ) {
+			oldStudent.get().setPhoto( file.getBytes() );
+		}
+		
+		return this.save( oldStudent.get() );
 	}
 }
